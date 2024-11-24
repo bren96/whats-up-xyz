81dcde6fb6d34af7057c3e34bb680e8a489193a0
bren96 <brendancullen996@gmail.com>
2024-11-23 23:45:50 -0500

    emails post api

---
+++ apps/whats-up-web/src/app/api/emails/route.ts
@@ -0,0 +1,14 @@
+import { prisma } from '@libs/whats-up-prisma';
+
+export function POST() {
+  // create email in db
+  prisma.email.create({
+    data: {
+      rawText: 'Test 123',
+    },
+  });
+
+  // create and run assistant thread
+  // validate response
+  // create events in db
+}
